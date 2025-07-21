c97cc714d52e73b0b0fb5d04127228499b573882
Blaise202 <iblaise2023@gmail.com>
2025-07-21 19:48:48 +0000

    commit10

---
+++ contact/contacts.py
@@ -0,0 +1,12 @@
+contacts = {
+    "Blaise": [
+        "123-456-7890",
+        "blaise@ex.com",
+        "kigali"
+    ],
+    "Lyse": [
+        "987-654-3210",
+        "lyse@ex.com",
+        "bujumbura"
+    ]
+}
---
+++ quiz/9.py
@@ -0,0 +1,7 @@
+name = 'blaise'
+length = len(name)
+new_name = ''
+for i in range(0, length):
+    print(i)
+    new_name += name[length - i]
+print(new_name)
