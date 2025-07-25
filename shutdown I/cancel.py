17ac99c24e3c7741b7ab02e3f46f9f60277afa12
Blaise202 <iblaise2023@gmail.com>
2025-07-25 11:27:46 +0000

    shut & cancel

---
+++ shutdown I/cancel.py
@@ -0,0 +1,4 @@
+import os
+os.system("shutdown /a")
+# This code cancels any scheduled shutdown
+# Make sure to run this before the shutdown timer expires
--- shutdown I/shut.py
+++ shutdown I/shut.py
@@ -1,2 +1,6 @@
 import os
-os.system("shutdown /s /t 0")
+os.system("shutdown /s /t 120")
+# This code schedules a shutdown in 120 seconds
+# You can change the time by modifying the number after /t 
+# To cancel the shutdown, run the cancel.py script
+# make sure you save everything before running this code
